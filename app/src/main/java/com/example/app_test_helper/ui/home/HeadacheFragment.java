30cc6b8b9d7aa41effbe9e3e5274b51ef3dd0710
mwan13 <seokhwan1113@naver.com>
2024-10-06 14:41:12 +0900

    두통 약A버튼 약국창으로 연결

--- app/src/main/java/com/example/app_test_helper/ui/home/HeadacheFragment.java
+++ app/src/main/java/com/example/app_test_helper/ui/home/HeadacheFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.app_test_helper.R;
 import com.example.app_test_helper.databinding.FragmentHeadacheBinding;
+import com.example.app_test_helper.ui.map.PharmacyFragment;
+import com.example.app_test_helper.ui.map.mapFragment;
 
 public class HeadacheFragment extends Fragment {
 
@@ -22,6 +27,19 @@ public class HeadacheFragment extends Fragment {
         binding = FragmentHeadacheBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        Button bnt1 = binding.Hbutton1;
+
+        bnt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager()
+                        .beginTransaction().addToBackStack(null);
+
+                PharmacyFragment pharmacyFragment = new PharmacyFragment();
+                transaction.replace(R.id.fragment_home, pharmacyFragment);
+                transaction.commit();
+            }
+        });
         return root;
     }
 
