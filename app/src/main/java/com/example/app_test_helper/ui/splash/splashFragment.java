c9ff6b62070d4b44fee31ba6b853d69e1046078d
Kimgyudo <kkh0000412@naver.com>
2024-10-07 15:20:35 +0900

    splash 수정

--- app/src/main/java/com/example/app_test_helper/ui/splash/splashFragment.java
+++ app/src/main/java/com/example/app_test_helper/ui/splash/splashFragment.java
@@ -1,17 +1,15 @@
 package com.example.app_test_helper.ui.splash;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import com.example.app_test_helper.MainActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import com.example.app_test_helper.R;
 
 public class splashFragment extends Fragment {
@@ -19,7 +17,6 @@ public class splashFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // fragment_splash_screen.xml 레이아웃을 설정
         return inflater.inflate(R.layout.fragment_splash_screen, container, false);
     }
 
@@ -27,16 +24,9 @@ public class splashFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // 3초 후에 MainActivity로 이동
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(getActivity(), MainActivity.class);
-            startActivity(intent);
-
-            // 트랜지션 애니메이션 추가 (페이드 인/아웃)
-            getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-
-            // 프래그먼트에서는 finish() 대신 액티비티를 종료하는 코드를 사용
-            getActivity().finish();
-        }, 3000); // 3000 밀리초 = 3초
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.nav_home); // nav_home 프래그먼트로 이동
+        }, 3000); // 3초 후에 홈 화면으로 이동
     }
 }
